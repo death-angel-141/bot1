211703b109bf96544f88bf11aabfd5af9361b9f1
death-angel-141 <61632737+death-angel-141@users.noreply.github.com>
2020-06-22 22:20:11 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -98,7 +98,7 @@ def get_readable_message():
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
                 if hasattr(download, 'is_torrent'):
                     msg += f"\n<b>Seeders</b>: <code>{download.aria_download().num_seeders}</code>" \
-                           f"& <b>Peers</b>: {download.aria_download().connections} "
+                           f" & <b>Peers</b>: {download.aria_download().connections} "
                 msg += f"\nGID: <code>{download.gid()}</code>"
             msg += "\n\n"
         return msg
